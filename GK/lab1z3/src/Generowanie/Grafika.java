520ba2534c527fdb999677579308e9d4b2e71615
Andrzej Dabrowski <andrzej.dabrowski94@gmail.com>
2017-10-15 23:03:44 +0200

    GKL1-008 laczenie przez maske

--- GK/lab1z3/src/Generowanie/Grafika.java
+++ GK/lab1z3/src/Generowanie/Grafika.java
@@ -1,5 +1,6 @@
 package Generowanie;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -8,8 +9,9 @@ import javax.imageio.ImageIO;
 
 public class Grafika {
 
-	private BufferedImage image;
-	private BufferedImage wczytany;
+	private BufferedImage maska;
+	private BufferedImage wczytany, obraz1, obraz2, dest;
+
 	private String name;
 
 	// Image resolution
@@ -22,7 +24,7 @@ public class Grafika {
 	}
 
 	public void init() {
-		image = new BufferedImage(x_res, y_res, BufferedImage.TYPE_INT_RGB);
+		maska = new BufferedImage(x_res, y_res, BufferedImage.TYPE_INT_RGB);
 	}
 
 	public void wczytaj(String name) {
@@ -32,20 +34,37 @@ public class Grafika {
 			wczytany = ImageIO.read(imageFile);
 			x_res = wczytany.getWidth();
 			y_res = wczytany.getHeight();
-			image = new BufferedImage(x_res, y_res, BufferedImage.TYPE_INT_RGB);
+			maska = new BufferedImage(x_res, y_res, BufferedImage.TYPE_INT_RGB);
 		} catch (IOException e) {
 			System.err.println("Blad odczytu obrazka");
 			e.printStackTrace();
 		}
 	}
 
+	public void wczytaj(String obraz1, String obraz2) {
 
+		File fo1 = new File(obraz1);
+		File fo2 = new File(obraz2);
+
+		try {
+			this.obraz1 = ImageIO.read(fo1);
+			this.obraz2 = ImageIO.read(fo2);
+			x_res = (this.obraz1.getWidth() < this.obraz2.getWidth()) ? this.obraz1.getWidth() : this.obraz2.getWidth();
+			y_res = (this.obraz1.getHeight() < this.obraz2.getHeight()) ? this.obraz1.getHeight()
+					: this.obraz2.getHeight();
+			maska = new BufferedImage(x_res, y_res, BufferedImage.TYPE_INT_RGB);
+			dest = new BufferedImage(x_res, y_res, BufferedImage.TYPE_INT_RGB);
+		} catch (IOException e) {
+			System.err.println("Blad odczytu obrazka");
+			e.printStackTrace();
+		}
+	}
 
 	private void copy() {
 		int i, j;
 		for (i = 0; i < y_res; i++)
 			for (j = 0; j < x_res; j++) {
-				image.setRGB(j, i, wczytany.getRGB(j, i));
+				maska.setRGB(j, i, wczytany.getRGB(j, i));
 			}
 
 	}
@@ -59,41 +78,56 @@ public class Grafika {
 
 		// Initialize an empty image, use pixel format
 		// with RGB packed in the integer data type
-		image = new BufferedImage(x_res, y_res, BufferedImage.TYPE_INT_RGB);
+		maska = new BufferedImage(x_res, y_res, BufferedImage.TYPE_INT_RGB);
 
 		// Process the image, pixel by pixel
 		for (i = 0; i < y_res; i++)
 			for (j = 0; j < x_res; j++) {
-				image.setRGB(j, i, k_t);
+				maska.setRGB(j, i, k_t);
 				if ((j + x_przes) % x <= size) {
-					image.setRGB(j, i, k_l);
+					maska.setRGB(j, i, k_l);
 				}
 				if ((i + y_przes) % y <= size) {
-					image.setRGB(j, i, k_l);
+					maska.setRGB(j, i, k_l);
 				}
 
 			}
 
 	}
-	
-	
+
+	private void polacz() {
+		int i, j,r,g,b;
+		Color m, o1, o2,d;
+		
+		for (i = 0; i < y_res; i++)
+			for (j = 0; j < x_res; j++) {
+				m = new Color(maska.getRGB(j, i));
+				o1 = new Color(obraz1.getRGB(j, i));
+				o2 = new Color(obraz2.getRGB(j, i));
+				r=(m.getRed()/255)*o1.getRed()+((255-m.getRed())/255)*o2.getRed();
+				g=(m.getGreen()/255)*o1.getGreen()+((255-m.getGreen())/255)*o2.getGreen();
+				b=(m.getBlue()/255)*o1.getBlue()+((255-m.getBlue())/255)*o2.getBlue();
+				d = new Color(r,g,b);
+				dest.setRGB(j, i, d.getRGB());
+
+			}
+
+	}
+
 	public void krata_na_obraz(int size, int x, int y, int x_przes, int y_przes, int r_l, int g_l, int b_l) {
 
 		int i, j;
 		int k_l = int2RGB(r_l, g_l, b_l);
-		
-
-		
 
 		// Process the image, pixel by pixel
 		for (i = 0; i < y_res; i++)
 			for (j = 0; j < x_res; j++) {
-				image.setRGB(j, i, wczytany.getRGB(j, i));
+				maska.setRGB(j, i, wczytany.getRGB(j, i));
 				if ((j + x_przes) % x <= size) {
-					image.setRGB(j, i, k_l);
+					maska.setRGB(j, i, k_l);
 				}
 				if ((i + y_przes) % y <= size) {
-					image.setRGB(j, i, k_l);
+					maska.setRGB(j, i, k_l);
 				}
 
 			}
@@ -129,10 +163,10 @@ public class Grafika {
 					// based on the ring index
 					if (r % 2 == 0)
 						// Even ring - set black color
-						image.setRGB(j, i, black);
+						maska.setRGB(j, i, black);
 					else
 						// Odd ring - set white color
-						image.setRGB(j, i, white);
+						maska.setRGB(j, i, white);
 				}
 			}
 	}
@@ -164,10 +198,10 @@ public class Grafika {
 				// based on the ring index
 				if (r % 2 == 0)
 					// Even ring - set black color
-					image.setRGB(j, i, black);
+					maska.setRGB(j, i, black);
 				else
 					// Odd ring - set white color
-					image.setRGB(j, i, white);
+					maska.setRGB(j, i, white);
 			}
 	}
 
@@ -199,10 +233,10 @@ public class Grafika {
 				// based on the ring index
 				if ((a / 15) % 2 == 0) {
 					// Even ring - set black color
-					image.setRGB(j, i, black);
+					maska.setRGB(j, i, black);
 				} else {
 					// Odd ring - set white color
-					image.setRGB(j, i, white);
+					maska.setRGB(j, i, white);
 				}
 			}
 	}
@@ -232,7 +266,7 @@ public class Grafika {
 
 				}
 
-				image.setRGB(j, i, szarosc(kolor));
+				maska.setRGB(j, i, szarosc(kolor));
 
 				// Make decision on the pixel color
 				// // based on the ring index
@@ -285,10 +319,10 @@ public class Grafika {
 				// based on the ring index
 				if (r % 2 == 0)
 					// Even ring - set black color
-					image.setRGB(j, i, black);
+					maska.setRGB(j, i, black);
 				else
 					// Odd ring - set white color
-					image.setRGB(j, i, white);
+					maska.setRGB(j, i, white);
 			}
 	}
 
@@ -319,17 +353,17 @@ public class Grafika {
 
 				if ((x / size) % 2 == 0) {
 					if ((y / size) % 2 == 1) {
-						image.setRGB(j, i, k_l);
+						maska.setRGB(j, i, k_l);
 					} else {
-						image.setRGB(j, i, k_t);
+						maska.setRGB(j, i, k_t);
 					}
 
 				} else {
 					if ((y / size) % 2 == 1) {
-						image.setRGB(j, i, k_t);
+						maska.setRGB(j, i, k_t);
 					} else {
 
-						image.setRGB(j, i, k_l);
+						maska.setRGB(j, i, k_l);
 					}
 				}
 
@@ -340,7 +374,17 @@ public class Grafika {
 	public void save(String name) {
 		// Save the created image in a graphics file
 		try {
-			ImageIO.write(image, "bmp", new File(name));
+			ImageIO.write(maska, "bmp", new File(name));
+			System.out.println(" image created successfully");
+		} catch (IOException e) {
+			System.out.println("The image cannot be stored");
+		}
+	}
+
+	public void saveDest(String name) {
+		// Save the created image in a graphics file
+		try {
+			ImageIO.write(dest, "bmp", new File(name));
 			System.out.println(" image created successfully");
 		} catch (IOException e) {
 			System.out.println("The image cannot be stored");
@@ -447,6 +491,29 @@ public class Grafika {
 		wczytaj(sourse);
 		krata_na_obraz(size, x, y, x_przes, y_przes, r_l, g_l, b_l);
 		save(destination);
+
+	}
+
+	public void procedura_szachownica_polacz(int size, int ank, String o1, String o2, String destanacion) {
+		wczytaj(o1, o2);
+		szachownica(size, ank, 255, 255, 255, 0, 0, 0);
+		polacz();
+		saveDest(destanacion);
+	}
+	
+	public void procedura_promien_polacz(int ilosc,String o1, String o2, String destanacion) {
+		wczytaj(o1, o2);
+		promien(ilosc);
+		polacz();
+		saveDest(destanacion);
+
+	}
+	
+	public void procedura_pierscienie_rozmyte_polacz(int szybkosc,int wielkosc, String o1, String o2, String destanacion) {
+		wczytaj(o1, o2);
+		pierscienierozmyte(szybkosc, wielkosc);
+		polacz();
+		saveDest(destanacion);
 
 	}
 
--- GK/lab1z3/src/Generowanie/Main.java
+++ GK/lab1z3/src/Generowanie/Main.java
@@ -20,6 +20,10 @@ public class Main {
 		g.procedura_krata(5, 50, 100, 20, 20, 255, 255,255, 0,0,0);
 		g.procedura_przepisz("lisc.bmp", "liscie.bmp");
 		g.procedura_dorysuj_krate("lisc.bmp", "liscieZakrata.bmp",5,50,50,20,40,255,0,0);
+		g.procedura_szachownica_polacz(50, 45, "lisc.bmp", "sam.bmp", "polaczszach.bmp");
+		g.procedura_promien_polacz(15, "lisc.bmp", "sam.bmp", "polaczprom.bmp");
+		g.procedura_pierscienie_rozmyte_polacz(20, 10,"lisc.bmp", "sam.bmp", "polaczrozm.bmp");
+		
 	
 	}
 
